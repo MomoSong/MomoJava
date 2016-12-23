b32d66956817c18f3be7fa3d335a9343daeb769e
MomoSong <johnmor78@gmail.com>
2016-12-24 02:31:42 +0900

    no message

--- Test/src/test.java
+++ Test/src/test.java
@@ -1,11 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by johnm on 2016-12-15.
  */
 public class test {
     public static void main(String[] args){
-        for(int i=0; i<20; i++){
-            System.out.println(   i%10     );
-        }
+        Scanner s = new Scanner(System.in);
+        String[] argArr = null;
+
+
+        String prompt = ">>";
+        System.out.print(prompt);
+
+        String input = s.nextLine();
+
+        input = input.trim();
+        System.out.println(input);
+
+        argArr = input.split(" +");
+        System.out.println(Arrays.toString(argArr));
+
+
+        String command = argArr[0].trim();
+        System.out.println(command);
+
+
+
 
     }
+
 }
